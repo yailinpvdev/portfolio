d05a95fcb9d0a21beb63d21891f7b295ab4ed362
Yailín <tu-correo@github.com>
2026-03-13 10:36:46 -0500

    feat: add AOS library for scroll animations

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+import AOS from "aos"
+import "aos/dist/aos.css"
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+AOS.init()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
