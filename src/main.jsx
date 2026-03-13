a6f3dccec43187512c4490817a61aa320f2f3cf5
Yailín <tu-correo@github.com>
2026-03-13 10:53:33 -0500

    fix: initialize AOS scroll animations

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
+
 import AOS from "aos"
 import "aos/dist/aos.css"
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+AOS.init({
+  duration: 800,
+  once: true,
+})
 
-AOS.init()
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
